a2a73cb31bc51e017302f5b0120261cb95e6d20c
robbcorrea <51095085+RobbCorrea@users.noreply.github.com>
2019-11-06 16:24:44 -0600

    tercer avance

---
+++ js/ImageLoading.js
@@ -0,0 +1,50 @@
+var carPic = document.createElement("img");
+var otherCarPic = document.createElement("img");
+var trackPics = [];
+
+var picsToLoad = 0; // set automatically based on imageList in loadImages()
+
+function countLoadedImagesAndLaunchIfReady() {
+picsToLoad--;
+console.log(picsToLoad);
+if(picsToLoad == 0) {
+ imageLoadingDoneSoStartGame();
+}
+}
+
+function beginLoadingImage(imgVar, fileName) {
+imgVar.onload = countLoadedImagesAndLaunchIfReady;
+imgVar.src = "images/"+fileName;
+}
+
+function loadImageForTrackCode(trackCode, fileName) {
+trackPics[trackCode] = document.createElement("img");
+beginLoadingImage(trackPics[trackCode], fileName);
+}
+
+function loadImages() {
+var imageList = [
+ {varName: carPic, theFile: "player1car.png"},
+ {varName: otherCarPic, theFile: "player2car.png"},
+
+ {trackType: TRACK_ROAD, theFile: "track_road.png"},
+ {trackType: TRACK_WALL, theFile: "track_wall.png"},
+ {trackType: TRACK_GOAL, theFile: "track_goal.png"},
+ {trackType: TRACK_TREE, theFile: "little_tree.png"},
+ {trackType: TRACK_FLAG, theFile: "track_flag.png"},
+ {trackType: TRACK_GARLEFT, theFile: "leftGard.png"},
+ {trackType: TRACK_GARIGHT, theFile: "rightGard.png"},
+ {trackType: CASC_LEFT, theFile: "cascLeft.png"},
+ {trackType: CASC_RIGHT, theFile: "cascRight.png"}
+ ];
+
+picsToLoad = imageList.length;
+
+for(var i=0;i<imageList.length;i++) {
+ if(imageList[i].varName != undefined) {
+  beginLoadingImage(imageList[i].varName, imageList[i].theFile);
+ } else {
+  loadImageForTrackCode(imageList[i].trackType, imageList[i].theFile);
+ }
+}
+}
